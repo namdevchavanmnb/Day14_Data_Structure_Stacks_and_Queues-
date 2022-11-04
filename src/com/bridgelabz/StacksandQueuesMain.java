4cfa3b7272deaf65eec9bdd6d8810ad0d4ed03b1
namdevchavanmnb <namdevchavanmnb@gmail.com>
2022-11-04 06:52:25 +0530

    Welcome

---
+++ src/com/bridgelabz/StacksandQueuesMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+import javax.xml.transform.stream.StreamSource;
+
+public class StacksandQueuesMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Stacks and Queues");
+    }
+}
