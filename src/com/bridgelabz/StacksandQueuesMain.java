0df0dcfde0a683a92d06df97107f88514e259274
namdevchavanmnb <namdevchavanmnb@gmail.com>
2022-11-07 06:14:49 +0530

    Added:Abilty to peak and pop

--- src/com/bridgelabz/StacksandQueuesMain.java
+++ src/com/bridgelabz/StacksandQueuesMain.java
@@ -5,5 +5,16 @@ import javax.xml.transform.stream.StreamSource;
 public class StacksandQueuesMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Stacks and Queues");
+        LinkedList <Integer>list=new LinkedList<>();
+        list.push(70);
+        list.push(30);
+        list.push(56);
+        list.show();
+        list.pop();
+        list.show();
+        list.peak(30);
+        list.show();
     }
 }
+
+
